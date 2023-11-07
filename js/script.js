1c3920e3b7ecbbf38f78161b7ee45ba925d626f8
abdllah07 <abdallahalhasan2@gmail.com>
2023-11-08 02:58:02 +0300

    first commit4

--- js/script.js
+++ js/script.js
@@ -35,7 +35,31 @@ $(function () {
     },
   });
 });
-
+$(function () {
+  $("#team-members1").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      480: {
+        items: 2,
+      },
+    },
+  });
+});
 /* =========================================
                 Progress Bars
 ============================================ */
