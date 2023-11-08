2bb94fdf25d8b5a325ea1ed5acfb701509829137
abdllah07 <abdallahalhasan2@gmail.com>
2023-11-09 02:11:42 +0300

    first commit5.3

--- js/whatUs.js
+++ js/whatUs.js
@@ -24,3 +24,26 @@ function hideAllButOne(index) {
         }
     }
 }
+  // Get the button
+  let mybutton1 = document.getElementById("myBtn2");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton1.style.display = "block";
+
+    } else {
+      mybutton1.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
